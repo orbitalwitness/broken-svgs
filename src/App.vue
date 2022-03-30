6059a6ac06864b2d26d61712a5aefe439023330b
Domagoj Vidovic <domagoj@orbitalwitness.com>
2022-03-30 12:58:27 +0100

    Create project

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>Inline</span>
+    <IconWord />
+    <span>External</span>
+    <img src="./assets/icon-word.svg" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import IconWord from './assets/icon-word.svg?inline'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    IconWord
   }
 }
 </script>
